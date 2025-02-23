87a758a2573b7fbba948fd208669b226b2a073f0
GiteshGayakwad <143732488+GiteshGayakwad@users.noreply.github.com>
2025-02-23 17:23:18 +0530

    Stirng Method added

--- OOP/Strings/StringMethods.java
+++ OOP/Strings/StringMethods.java
@@ -1,75 +1,47 @@
 package OOP.Strings;
 
+import java.util.Arrays;
+
 public class StringMethods {
     public static void main(String[] args){
-        String s1 = "India";
-        String s2 = "Hello";
 
-        // find  length of string
-//        findLengthMethod(s1, s2);
-
-        // use charAt() - find char at which place it located
-//        charAtMethod(s1);
-
-        // get subString from String
-//        subStringMethod(s1, s2);
-
-        // concatenates string at the end of that String
-//        concatMethod(s1,s2);
-
-        // find an indexOf character
-//        indexOfMethod(s1,s2);
-
-        // find string indexOf at after specific index
-//        indexOfSpecificIndexStartFromMethod(s1,s2);
+        String s1 = "Hello";
+        String s2 = " H  ell  o  ";
+        String s3 = "Hello";
+        String s4 = "HelloWorld";
 
 
-        lastIndexOfString(s1,s2);
-
-
+        System.out.println(s1.length()); // 5
+        System.out.println(s1.charAt(0)); // H
+        System.out.println(s1.substring(2)); // llo
+        System.out.println(s1.substring(1,4)); // ell
+        System.out.println(s1.indexOf('l')); // 2
+        System.out.println(s1.lastIndexOf('l')); // 3
+        System.out.println(s1.toLowerCase()); // hello
+        System.out.println(s1.toUpperCase()); // HELLO
+        System.out.println(s2.trim()); // "Hello"
+        System.out.println(s1.equals(s2)); // false
+        System.out.println(s1.equals(s3)); // true
+        System.out.println(s1.equalsIgnoreCase(s2)); // false
+        System.out.println(s1.contains("l")); // true
+        System.out.println(s2.contains(" ")); // true
+        System.out.println(s3.contains("H")); // true
+        System.out.println(s1.startsWith("H")); // true
+        System.out.println(s1.startsWith("l")); // false
+        System.out.println(s1.endsWith("o"));  // true
+        System.out.println(s1.replace("l" , "i")); // Heiio
+        System.out.println(s1.replaceAll("l", "o"));  // Heooo
+        System.out.println(s1.split(" "));
+        System.out.println(s1.join("," , "Hello" , " world"));
+        System.out.println(s1.valueOf(123));
+        System.out.println(s1.isEmpty()); // false
+        System.out.println(String.format("Hello %s", "World"));
+        System.out.println(s1.compareTo(s2)); // 40
+        System.out.println(s1.compareTo(s3)); // 0
+        System.out.println(s1.compareToIgnoreCase(s2)); // 72
+        char[] sArr = s1.toCharArray();
+        System.out.println(Arrays.toString(sArr));
+        System.out.println(s1.repeat(5));
     }
-
-
-    public static void charAtMethod(String s){
-        System.out.println(s.charAt(2)); // d
-        System.out.println(s.charAt(0)); // I
-        // System.out.println(s.charAt(5)); // StringIndexOutOfBoundException
-    }
-
-    public static void findLengthMethod(String s1, String s2){
-        System.out.println(s1.length());
-        System.out.println(s2.length());
-    }
-
-    public static void subStringMethod(String s1, String s2){
-        System.out.println(s1.substring(1)); // ndia
-        System.out.println(s2.substring(3)); // lo
-    }
-
-    public static void concatMethod(String s1, String s2){
-        System.out.println(s1.concat(s2)); // IndiaHello
-        System.out.println(s2.concat(s1)); // HelloIndia
-    }
-
-    public static void indexOfMethod(String s1, String s2){
-        System.out.println(s1.indexOf("i")); // 3
-        System.out.println(s1.indexOf("l")); // if not present in string print -1
-        System.out.println(s2.indexOf("o")); // 4
-        System.out.println(s2.indexOf("I")); // if not present in string print -1
-    }
-
-    public static void indexOfSpecificIndexStartFromMethod(String s1, String s2){
-        System.out.println(s1.indexOf("i", 2)); // 3
-        System.out.println(s1.indexOf("l", 1)); // if not present in string print -1
-        System.out.println(s2.indexOf("o", 6)); // -1
-        System.out.println(s2.indexOf("e", 1)); // if not present in string print -1
-    }
-
-
-    public static void lastIndexOfString(String s1, String s2){
-        System.out.println(s1.lastIndexOf("I")); // 0
-        System.out.println(s2.lastIndexOf("l")); // 3
-    }
-
 
 }
